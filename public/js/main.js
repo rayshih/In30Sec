ee45d761593775bf96ca76cce94bc094ff2709c1
Ray Shih <mnf.shih@gmail.com>
2013-07-13 02:26:39 -0700

    add mustache

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
 $(function (){
-  alert('Hello world');
+  var rect = new Rect(3, 4, 5, 6);
+  var headline = new HeadLine(rect, {hello: 'world'});
+
+  var widgets = [headline];
+
+  var toolbarController = new ToolBarController($('#toolbar'));
+  var editorController = new EditorController($('#edit-area'));
+
+  toolbarController.setWidgets(widgets);
 });
